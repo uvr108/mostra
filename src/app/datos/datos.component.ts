4afe0dcf4687f41b4968ce75bf6c43857d28c666
osito <uvergara@csn.uchile.cl>
2019-05-17 17:27:30 -0400

    Se eliminan comentarios

--- src/app/datos/datos.component.ts
+++ src/app/datos/datos.component.ts
@@ -17,9 +17,10 @@ export class DatosComponent implements OnInit {
   ratios: Object;
   zona : Array<string>;
 
-  cabecera: Array<string> = ['sfile','latitud','longitud','dep','m1_magnitud',
+  cabecera3: Array<string> = ['sfile','latitud','longitud','dep','m1_magnitud',
   'm1_tipo','m5','m20','no','pup','retardo','sensible','tipo_estadistica','version'];
   cabecera2 : Array<String> = ['yr','jl','data'];
+  cabecera : Array<String> = ['zona','fecha_origen','sfile','latitud','longitud', 'no','cont_5','cont_10','cont_15','cont_20'];
 
 
   
@@ -41,7 +42,7 @@ export class DatosComponent implements OnInit {
   carga_datos(period:string, zona:Array<string>) {
     console.log(`PERIOD : ${JSON.stringify(period)}`); 
     var between =   {'initial': period['fecha_ini'] + 'T00:00:00+00:00' ,'final': period['fecha_fin']+'T23:59:59+00:00','index': 'fecha_origen'}
-    var orderBy = {'order': ['sfile','version'] , 'tipo':'desc'};
+    var orderBy = {'order': ['zona','fecha_origen'] , 'tipo':'desc'};
    
     console.log(`ZONA : ${zona}`);
 
@@ -49,7 +50,7 @@ export class DatosComponent implements OnInit {
 
     console.log( `between : ${JSON.stringify(between)}`);
     const mensaje: Message = {'command': 'listar','tipo': 'rethink', 
-      'message': [{'command': 'listar', 'message': {'table': 'triggers', 'option': 'select', 'betweenISO':between,'orderBy': orderBy}}]};
+      'message': [{'command': 'listar', 'message': {'table': 'informes', 'option': 'select', 'betweenISO':between,'orderBy': orderBy}}]};
 
     console.log(`Message : ${JSON.stringify(mensaje)}`);
 

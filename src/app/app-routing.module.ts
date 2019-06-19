e0f6607592ca89c5d32dc538a7480fdff9f6de7f
osito <uvergara@csn.uchile.cl>
2019-06-19 11:59:20 -0400

    Se eliminaron comentarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,18 @@ import { DatosComponent } from './datos/datos.component';
 import { EstacionesComponent } from './estaciones/estaciones.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { RatiosComponent } from './ratios/ratios.component';
+import { AnalisisComponent} from './analisis/analisis.component';
 
 const routes: Routes = [
+  
+  { path: '', redirectTo: '/menu', pathMatch: 'full' },
   { path:'menu', component: MenuComponent},
   { path:'datos', component: DatosComponent},
+  { path:'analisis', component: AnalisisComponent},
   { path:'estaciones', component: EstacionesComponent},
   { path:'eventos', component: EventosComponent},
-  { path:'ratios', component: RatiosComponent},
-  { path: '', redirectTo: 'menu', pathMatch: 'full' },
+  { path:'ratios', component: RatiosComponent}
+  
 
 ];
 

af6d0b943298f49a76004f9ef57b4f4407036440
osito <uvergara@csn.uchile.cl>
2019-07-09 09:56:58 -0400

    Se implementa bajar datos

--- src/app/analisis/analisis.component.ts
+++ src/app/analisis/analisis.component.ts
@@ -3,7 +3,7 @@ import {NgxPaginationModule} from 'ngx-pagination';
 import { WebsocketService } from '../websocket.service';
 import { ConectaService } from '../conecta.service';
 import { Message } from '../message';
-import { FormBuilder } from '@angular/forms';
+import { Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-analisis',
@@ -18,28 +18,85 @@ export class AnalisisComponent implements OnInit {
   ratios: Object;
   zona : Array<string>;
 
+  p : any; 
+
+  make_period() {
+
+    this.fecha_ini = this.periodForm.value['fecha_ini'];
+    this.fecha_fin = this.periodForm.value['fecha_fin'];
+
+    return [this.fecha_ini , this.fecha_fin]
+  }
+
+  tipo='tabla';
+
+  periodForm = this.fb.group({
+
+    fecha_ini : ['', [Validators.required]],
+    fecha_fin : ['',  [Validators.required]],
+    opt_n : ['1'],
+    opt_nc : ['1'],
+    opt_v : ['1'],
+    opt_zc : ['1'],
+    opt_s : ['1'], 
+    opt_es : ['1'],
+  });
+
+  fecha_ini : string;
+  fecha_fin : string;
+
+  filedir:string;
+
   // cabecera : Array<String> = ['sfile','action','latitud','longitud','m1_magnitud','m1_magnitud','m1_tipo','m5','m20','no','operator','email_origen','retardo','sensible','tipo_estadistica','up','version'];
-  cabecera : Array<String> = ['sfile','action','fecha_origen','operator','latitud','longitud','m1_magnitud','no','m5','m20','email_origen','retardo','sensible','tipo_estadistica','up','version'];
+  cabecera : Array<String> = ['sfile','action','fecha_origen','operator','latitud','longitud','m1_magnitud','m1_tipo','no','m5','m20','email_origen','retardo','sensible','tipo_estadistica','up','version'];
 
   constructor(private fb: FormBuilder, private dttService: ConectaService) { 
     this.dttService.stream_msg.subscribe(
       msg => {
   
-        console.log('Resiviendo datos analisis');
-        console.log(msg);
-        this.tabla = msg;
+
+        if (Object.keys(msg).indexOf('filedir') == -1) { 
+          this.tabla = msg;
+        }
+        else { 
+          console.log(`filedir : ${JSON.stringify(msg)}`);
+          this.filedir=msg['filedir'];
+   
+        }    
       });
+
+  
   }
     ngOnInit() {
   }
 
-  carga_datos(period:string) {
-  
-    var between = [period['fecha_ini'] + 'T00:00:00+00:00' , period['fecha_fin']+'T23:59:59+00:00','fecha_origen'];
-    var order = {'version':'desc'};
-    var pluck = {'fecha_origen':['sfile','action','fecha_origen','latitud','longitud','m1_magnitud',
-    'm1_magnitud','m1_tipo','m5','m20','no','operator','email_origen','retardo','sensible','tipo_estadistica','up','version']}
+  pull() {
 
+    this.tipo='file';
+  
+    const mensaje: Message = {'command': 'download_mostra', 'tipo': 'csv','message': this.tabla};
+  
+    console.log(`Message : ${JSON.stringify(mensaje)}`);
+  
+    this.dttService.send(mensaje);
+  
+    }
+
+  onSubmit() {
+  
+    this.tipo='tabla'; 
+
+    var period = this.make_period();
+  
+    console.log(`period : ${period}`);
+  
+    
+    var between = [period[0] + 'T00:00:00+00:00' , period[1]+'T23:59:59+00:00','fecha_origen'];
+
+
+    var between = [period[0] + 'T00:00:00+00:00' , period[1]+'T23:59:59+00:00','fecha_origen'];
+    var order = {'fecha_origen':'desc','version':'desc'};
+    
     const mensaje: Message = {'command': 'listar', 'tipo': 'rethink',
     'message': {'table': 'analisis', 'option': 'select','betweenISO':between, 'order': order}};
 
